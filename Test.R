87c28bc8660e2c5e028d0000293f853000f7a769
A. Immanuel Blessy <immanuelblessy7@gmail.com>
2020-07-12 16:16:19 +0530

    Detailing the steps to link RStudio with Git, if not done in the first place.

--- Test.R
+++ Test.R
@@ -1,1 +1,32 @@
-print("Praise the Lord")
+a = print("Praise the Lord")
+# ###
+# steps to do this:
+#   1. Navigate to the working dir
+#   2. git init
+#       Initialized empty Git repository in /Users/immanuelblessy/R_WorkSpace/Prior_to_linking_to_version_control/.git/
+    # 3. git add --all
+    # 4. git commit -m "Initial Commit"
+    #     [master (root-commit) 17e5b9b] Initial Commit
+    #     10 files changed, 55 insertions(+)
+    #     create mode 100644 .Rproj.user/B5E9E895/build_options
+    #     create mode 100644 .Rproj.user/B5E9E895/sources/prop/3284CCBB
+    #     create mode 100644 .Rproj.user/B5E9E895/sources/prop/INDEX
+    #     create mode 100644 .Rproj.user/B5E9E895/sources/s-43682B92/59D6C0A6
+    #     create mode 100644 .Rproj.user/B5E9E895/sources/s-43682B92/59D6C0A6-contents
+    #     create mode 100644 .Rproj.user/B5E9E895/sources/s-43682B92/lock_file
+    #     create mode 100644 .Rproj.user/shared/notebooks/patch-chunk-names
+    #     create mode 100644 .Rproj.user/shared/notebooks/paths
+    #     create mode 100644 Prior_to_linking_to_version_control.Rproj
+    #     create mode 100644 Test.R
+    # 5. git push -u origin master  
+    #     Enumerating objects: 17, done.
+    #     Counting objects: 100% (17/17), done.
+    #     Delta compression using up to 8 threads
+    #     Compressing objects: 100% (14/14), done.
+    #     Writing objects: 100% (17/17), 1.73 KiB | 886.00 KiB/s, done.
+    #     Total 17 (delta 0), reused 0 (delta 0)
+    #     To https://github.com/immanuelblessy/Prior_to_linking_to_version_control.git
+    #     * [new branch]      master -> master
+    #     Branch 'master' set up to track remote branch 'master' from 'origin'.
+        
+####
